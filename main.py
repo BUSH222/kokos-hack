f86f3166114bd2083f16ef4d708f38809d2bf798
kust <vad.velikanov@yandex.ru>
2024-10-06 01:08:47 +0300

    endpoint 1 front

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/admin_panel_login')
+def admin_panel_login():
+    return render_template("admin_panel_login.html")
+
+@app.route('/admin_panel/logs')
+def admin_panel_logs():
+    return render_template("admin_panel_logs.html")
+
 if __name__ == '__main__':
-    pass  # Do something
+    app.run(host='0.0.0.0', port=8000)
