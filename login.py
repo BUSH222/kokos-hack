223a9980df8a8d1c70c3832802f765581fa89aa6
nexxy06 <nikpan254@gmail.com>
2024-10-07 19:12:12 +0300

    yandex

--- login.py
+++ login.py
@@ -103,60 +103,23 @@ def yandex_callback():
         headers={'Authorization': f'OAuth {token}'}
     )
     user_info = user_info_response.json()
-    user_name = user_info.get('real_name', 'Имя пользователя не найдено')
+    user_name = user_info.get('display_name', 'absent')
+    unique_id = user_info.get('id', 'absent')
+    user_email = user_info.get('default_email', 'absent')
 
-    return f'Ваш токен: {token}<br>Ваше имя: {user_name}'
-
-
-    response = requests.get("http://www.example.com/token")
-    response.status_code
-    print(f"DDDDDDDDDDDDDDDDDDDDD{response}")
-    # token = yandex.authorize_access_token()
-    # user_info = yandex.get('https://login.yandex.ru/info', token=token)
-    # user = user_info.json()
-    # print(user)
+    cur.execute("SELECT id, name, password, email FROM users WHERE name = %s", (user_name,))
+    user_data = cur.fetchone()
+    if user_data:
+        user = User(*user_data)
+        login_user(user)
+    else:
+        cur.execute('INSERT INTO users(name, password, email) VALUES (%s, %s, %s) RETURNING id, name, password, email', 
+                (user_name, unique_id, user_email))
+        conn.commit()
+        new_user_data = cur.fetchone()
+        new_user = User(*new_user_data)
+        login_user(new_user)
     return redirect(url_for('account'))
-    # response = yandex.authorized_response()
-    # if response is None or response.get('access_token') is None:
-    #     return 'Access denied: reason={} error={}'.format(
-    #         request.args['error_reason'],
-    #         request.args['error_description']
-    #     )
-    # session['yandex_token'] = (response['access_token'], '')
-    # user_info = yandex.get('https://login.yandex.ru/info')
-    # return 'Logged in as: ' + user_info.data['login']
-# def yandex_callback():
-#     code = request.args.get('code')
-#     token_url = YANDEX_DISCOVERY_URL
-#     data = {
-#         'grant_type': 'authorization_code',
-#         'code': code,
-#         'client_id': YANDEX_CLIENT_ID,
-#         'client_secret': YANDEX_CLIENT_SECRET,
-#         'redirect_uri': YANDEX_REDIRECT_URI
-#     }
-#     # response = requests.get(token_url) # ,data=data
-#     # response.status_code
-#     # print(f"DDDDDDDDDDDDDDDDDDDDD{response}")
-#     # access_token = request.args.get('access_token')
-#     # response = session.post(token_url, data=data)
-#     # token_info = response.json()
-#     # access_token = token_info['access_token']
-
-#     # user_info_url = 'https://login.yandex.ru/info'
-#     # headers = {'Authorization': f'OAuth {access_token}'}
-#     # user_info_response = requests.post(user_info_url, headers=headers)
-#     # user_info = user_info_response.json()
-#     google_provider_cfg = requests.get("https://api.webmaster.yandex.net/v4/user").json()
-#     print(google_provider_cfg)
-#     real_name = request.args.get('real_name')
-#     print(f"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA{real_name}")
-#     # session['user'] = {
-#     #     'fio': user_info.get('real_name'),
-#     #     'avatar': user_info.get('default_avatar_id')
-#     # }
-
-#     return redirect(url_for('account'))
 
 @app.route('/login_gmail', methods=['GET', 'POST'])
 def login_gmail():
@@ -198,23 +161,20 @@ def callback():
 
     if userinfo_response.json().get("email_verified"):
         unique_id = userinfo_response.json()["sub"]
-        users_email = userinfo_response.json()["email"]
+        user_email = userinfo_response.json()["email"]
         picture = userinfo_response.json()["picture"]
         username = userinfo_response.json()["given_name"]
     else:
         return "User email not available or not verified by Google.", 400
     
-    # user = User(username=username, password=unique_id, email=users_email)
     cur.execute("SELECT id, name, password, email FROM users WHERE name = %s", (username,))
     user_data = cur.fetchone()
     if user_data:
-        # if not User.get(unique_id):
-        # User.create(unique_id, users_name, users_email)
         user = User(*user_data)
         login_user(user)
     else:
         cur.execute('INSERT INTO users(name, password, email) VALUES (%s, %s, %s) RETURNING id, name, password, email', 
-                (username, unique_id, users_email))
+                (username, unique_id, user_email))
         conn.commit()
         new_user_data = cur.fetchone()
         new_user = User(*new_user_data)
