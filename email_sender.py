eca4d041714b8646fabc1e029cae8fb603d7406a
pl1ght <m.emelyancev@voskhod.ru>
2024-10-07 21:06:24 +0300

    pep8 fix

--- email_sender.py
+++ email_sender.py
@@ -5,15 +5,17 @@ import requests
 
 settings = get_processor_settings()
 
-APIKEY=settings["APIKEY"]
-NAMESPACE=settings["APINAMESPACE"]
-def send_email_message_via_smtp(dest_email,message):
+APIKEY = settings["APIKEY"]
+NAMESPACE = settings["APINAMESPACE"]
+
+
+def send_email_message_via_smtp(dest_email, message):
     smtpObj = smtplib.SMTP('smtp.yandex.ru', 587)
     smtpObj.starttls()
-    smtpObj.login(settings['admin_mail'],settings['admin_mail_password'])
+    smtpObj.login(settings['admin_mail'], settings['admin_mail_password'])
     try:
-        log_event("trying to send a email message",10,dest_email=dest_email,message=message)
-        smtpObj.sendmail(settings['admin_mail'],dest_email, message)
+        log_event("trying to send a email message", 10, dest_email=dest_email, message=message)
+        smtpObj.sendmail(settings['admin_mail'], dest_email, message)
     except:
-        log_event("failed to send a email message",30,dest_email=dest_email)
+        log_event("failed to send a email message", 30, dest_email=dest_email)
     smtpObj.quit()
