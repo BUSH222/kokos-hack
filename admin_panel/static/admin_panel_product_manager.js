52276d4933a6c4eb58d47ea7940984d8d7865692
Ted <58144503+BUSH222@users.noreply.github.com>
2024-10-09 21:06:09 +0300

    style changes

---
+++ admin_panel/static/admin_panel_product_manager.js
@@ -0,0 +1,7 @@
+function clearInputs() {
+    document.getElementById('product_id').value = '';
+    document.getElementById('product_name').value = '';
+    document.getElementById('product_price').value = '';
+    document.getElementById('product_description').value = '';
+    document.getElementById('picture_url').value = '';
+}
